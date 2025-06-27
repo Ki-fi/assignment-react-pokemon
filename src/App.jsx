27ca56d87d6cbc956b3d22b781cd110fedbc851e
Ki-fi <k.meekels@gmail.com>
2025-06-27 12:06:07 +0200

    renderen van content gerepareerd

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ import axios from "axios";
 
 function App() {
 
-const [pokemon, setPokemon] = useState({});
-const [pokemonInfo, setPokemonInfo] = useState({});
+const [pokemonList, setPokemonList] = useState([]);
+const [pokemonDetails, setPokemonDetails] = useState([]);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
+const [page, setPage] = useState(0);
+const offset = page * 20;
 
     useEffect(() => {
 
-        async function getPokemon() {
-            try { const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-                setLoading(true);
-                setPokemon(response.data);
+        async function catchEmAll() {
+            setLoading(true);
+            try { const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+                const results = response.data.results;
+
+                const details = [];
+                for (const pokemon of results) {
+                    const res = await axios.get(pokemon.url);
+                    details.push(res.data);
+                }
+
+                setPokemonList(results);
+                setPokemonDetails(details);
+
             } catch(e) {
                 console.error(e);
             } finally {
@@ -23,45 +35,41 @@ const [error, setError] = useState(null);
             }
         }
 
-        async function getPokemonInfo() {
-            try { const response = await axios.get(`${pokemon.url}/`);
-                setLoading(true);
-                setPokemonInfo(response.data);
-            } catch(e) {
-                console.error(e);
-            } finally {
-                setLoading(false);
-            }
-        }
+        catchEmAll();
 
-    }, []);
+    }, [page]);
 
   return (
     <>
+      <div className="page-container">
       <h1>Gotta catch em all!</h1>
-        <button
-            type="button"
-            onClick={() => {}}
-        >Vorige</button>
-        <button
-            type="button"
-            onClick={() => {}}
-        >Vorige</button>
+          <div className="buttons">
+            <button
+                type="button"
+                onClick={() => {setPage(page - 1)}}
+                disabled={page === 0}
+            >Vorige</button>
+            <button
+                type="button"
+                onClick={() => {setPage(page + 1)}}
+            >Vorige</button>
+          </div>
 
-
-        {pokemon && pokemon.length > 0 && (
-            const result = pokemon.map((pokemon) => (
-                return(
+        <div className="pokemon-container">
+        {pokemonDetails && pokemonDetails.length > 0 && pokemonDetails.map((pokemon) => (
                     <Card
+                        key={pokemon.id}
                         name={pokemon.name}
-                        imgUrl={pokemonInfo.}
-                        nrOfMoves={pokemonInfo.}
-                        weight={pokemonInfo.}
-                        ability={pokemonInfo.}
+                        imgUrl={pokemon.sprites.front_default}
+                        nrOfMoves={pokemon.moves.length}
+                        weight={pokemon.weight}
+                        ability={pokemon.abilities[0]?.ability.name}
                     />
             )
-            ))
-        )}
+        )
+        }
+        </div>
+        </div>
 </>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,11 @@ import './Card.css';
 
 function Card({ name, imgUrl, nrOfMoves, weight, ability }) {
     return (
-        <article>
-            <h2>{name}</h2>
+        <article className="card">
+            <h3>{name}</h3>
             <img src={imgUrl} alt="Image of pokemon"/>
             <p><strong>Moves: </strong>{nrOfMoves}</p>
-            <p><strong>Weight:</strong>{weight}</p>
+            <p><strong>Weight: </strong>{weight}</p>
             <p><strong>Abilities:</strong></p>
             <div className="chip">{ability}</div>
         </article>
